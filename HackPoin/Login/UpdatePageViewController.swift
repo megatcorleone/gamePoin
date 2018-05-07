d4b5ecb62efd645ce84ce413e902e72cea09f2f8
megatcorleone <megatcorleone000@gmail.com>
2018-05-07 12:10:18 +0800

    Update Page

---
+++ HackPoin/Login/UpdatePageViewController.swift
@@ -0,0 +1,25 @@
+//
+//  UpdatePageViewController.swift
+//  HackPoin
+//
+//  Created by Globaltium on 03/05/2018.
+//  Copyright © 2018 Megat. All rights reserved.
+//
+
+import UIKit
+
+class UpdatePageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+
+    
+  
+
+}
---
+++ HackPoin/Main Menu/MainMenuViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MainMenuViewController.swift
+//  HackPoin
+//
+//  Created by Globaltium on 03/05/2018.
+//  Copyright © 2018 Megat. All rights reserved.
+//
+
+import UIKit
+import ASHorizontalScrollView
+import LGSideMenuController
+
+class MainMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+        
+    }
+
+
+    
+
+    
+}
